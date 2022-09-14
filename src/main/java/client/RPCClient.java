79f28bfe28523e73fbb0598d8f2636938728bb7c
UliGarci <20203tn022@utez.edu.mx>
2022-09-13 21:16:17 -0500

    Add three exercise

---
+++ src/main/java/client/RPCClient.java
@@ -0,0 +1,59 @@
+package client;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import server.Methods;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class RPCClient {
+    public static void main(String[] args) throws IOException, XmlRpcException {
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        config.setServerURL(new URL("http://localhost:1200"));
+        XmlRpcClient client = new XmlRpcClient();
+        client.setConfig(config);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elija un ejercicio:\n1.-Ejercicio 2\n2.-Ejercicio 3\n3.-Ejercicio 4");
+        int op = sc.nextInt();
+        switch(op) {
+            case 1:
+                System.out.println("Ingrese el primer numero:");
+                double num1 = sc.nextDouble();
+                System.out.println("Ingrese el segundo numero:");
+                double num2 = sc.nextDouble();
+                System.out.println("Ingrese el tercer numero:");
+                double num3 = sc.nextDouble();
+                System.out.println("Ingrese el cuarto numero:");
+                double num4 = sc.nextDouble();
+                Object[] ope = {num1,num2,num3,num4};
+                Object[] response = (Object[]) client.execute("Methods.resultados",ope);
+                System.out.println("Hola\nel producto es: "+response[0]+",\nla suma es: "+response[1]+
+                        ",\ny el promedio es: "+response[2]);
+                break;
+            case 2:
+                System.out.println("Ingresa el limite inferior ");
+                int num1E3 = sc.nextInt();
+                System.out.println("Ingrese el limite superior: ");
+                int num2E3 = sc.nextInt();
+                Object[] numbers = { num1E3,num2E3};
+                int responseE3 = (int) client.execute("Methods.sum",numbers);
+                System.out.println("RESULT -> "+responseE3);
+                break;
+            case 3:
+                int x;
+                Object[] num=new Object[5];
+                for(x=0;x<5;x++){
+                    System.out.println("Ingrese un numero:");
+                    num[x]=sc.nextInt();
+                }
+                Object[] arreglo = (Object[]) client.execute("Methods.arreglo",new Object[]{num});
+                for(x=0;x<5;x++){
+                    System.out.print(arreglo[x]+" ");
+                }
+                break;
+        }
+    }
+}
---
+++ src/main/java/server/Methods.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.util.Arrays;
+
+public class Methods {
+    public Object resultados(double num1, double num2, double num3, double num4){
+            double prod=num1*num2*num3*num4;
+            double sum=num1+num2+num3+num4;
+            double prom=sum/4;
+            Object[] result={prod,sum,prom};
+        return result;
+    }
+
+    public int sum(int num1, int num2){
+        int tot=0;
+        while(num1<num2){
+            num1++;
+            if(num1!=num2){
+                tot+=num1;
+            }
+        }
+        return tot;
+    }
+
+    public Object[] arreglo(Object[] num){
+        Arrays.sort(num);
+        return num;
+    }
+}
